326bee58471a157da60f33f26fe39e9bee91f67f
martoff <martoff@ukr.net>
2026-07-26 02:19:53 +0300

    add Cart's model

---
+++ src/models/auth.py
@@ -0,0 +1,175 @@
+import enum
+from datetime import datetime
+from typing import Optional, List, TYPE_CHECKING
+from sqlalchemy import (
+    Integer,
+    String,
+    Boolean,
+    DateTime,
+    ForeignKey,
+    Enum as SQLEnum
+)
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+
+if TYPE_CHECKING:
+    from src.models.cart import Cart
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class UserGroupEnum(str, enum.Enum):
+    USER = "USER"
+    MODERATOR = "MODERATOR"
+    ADMIN = "ADMIN"
+
+
+class GenderEnum(str, enum.Enum):
+    MAN = "MAN"
+    WOMAN = "WOMAN"
+
+
+class UserGroup(Base):
+    __tablename__ = "user_groups"
+
+    id: Mapped[int] = mapped_column(
+        Integer,
+        primary_key=True,
+        autoincrement=True
+    )
+    name: Mapped[UserGroupEnum] = mapped_column(
+        SQLEnum(UserGroupEnum), unique=True, nullable=False
+    )
+
+    users: Mapped[List["User"]] = relationship(back_populates="group")
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(
+        Integer,
+        primary_key=True,
+        autoincrement=True
+    )
+    email: Mapped[str] = mapped_column(
+        String,
+        unique=True,
+        index=True,
+        nullable=False
+    )
+    hashed_password: Mapped[str] = mapped_column(String, nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=False)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime,
+        default=datetime.utcnow
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime, default=datetime.utcnow, onupdate=datetime.utcnow
+    )
+    group_id: Mapped[int] = mapped_column(
+        ForeignKey("user_groups.id"),
+        nullable=False
+    )
+
+    group: Mapped["UserGroup"] = relationship(back_populates="users")
+    profile: Mapped[Optional["UserProfile"]] = relationship(
+        back_populates="user", cascade="all, delete-orphan"
+    )
+    activation_token: Mapped[Optional["ActivationToken"]] = relationship(
+        back_populates="user", cascade="all, delete-orphan"
+    )
+    password_reset_token: Mapped[
+        Optional["PasswordResetToken"]
+    ] = relationship(
+        back_populates="user", cascade="all, delete-orphan"
+    )
+    refresh_tokens: Mapped[List["RefreshToken"]] = relationship(
+        back_populates="user", cascade="all, delete-orphan"
+    )
+
+    cart: Mapped["Cart"] = relationship(
+        "Cart",
+        back_populates="user",
+        cascade="all, delete-orphan",
+        # passive_deletes=True
+    )
+
+
+class UserProfile(Base):
+    __tablename__ = "user_profiles"
+
+    id: Mapped[int] = mapped_column(
+        Integer,
+        primary_key=True,
+        autoincrement=True
+    )
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey("users.id"), unique=True, nullable=False
+    )
+    first_name: Mapped[Optional[str]] = mapped_column(String, nullable=True)
+    last_name: Mapped[Optional[str]] = mapped_column(String, nullable=True)
+    avatar: Mapped[Optional[str]] = mapped_column(String, nullable=True)
+    gender: Mapped[Optional[GenderEnum]] = mapped_column(
+        SQLEnum(GenderEnum), nullable=True
+    )
+    date_of_birth: Mapped[Optional[datetime]] = mapped_column(
+        DateTime,
+        nullable=True
+    )
+    info: Mapped[Optional[str]] = mapped_column(String, nullable=True)
+
+    user: Mapped["User"] = relationship(back_populates="profile")
+
+
+class ActivationToken(Base):
+    __tablename__ = "activation_tokens"
+
+    id: Mapped[int] = mapped_column(
+        Integer,
+        primary_key=True,
+        autoincrement=True
+    )
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey("users.id"), unique=True, nullable=False
+    )
+    token: Mapped[str] = mapped_column(String, unique=True, nullable=False)
+    expires_at: Mapped[datetime] = mapped_column(DateTime, nullable=False)
+
+    user: Mapped["User"] = relationship(back_populates="activation_token")
+
+
+class PasswordResetToken(Base):
+    __tablename__ = "password_reset_tokens"
+
+    id: Mapped[int] = mapped_column(
+        Integer,
+        primary_key=True,
+        autoincrement=True
+    )
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey("users.id"), unique=True, nullable=False
+    )
+    token: Mapped[str] = mapped_column(String, unique=True, nullable=False)
+    expires_at: Mapped[datetime] = mapped_column(DateTime, nullable=False)
+
+    user: Mapped["User"] = relationship(back_populates="password_reset_token")
+
+
+class RefreshToken(Base):
+    __tablename__ = "refresh_tokens"
+
+    id: Mapped[int] = mapped_column(
+        Integer,
+        primary_key=True,
+        autoincrement=True
+    )
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey("users.id"),
+        nullable=False
+    )
+    token: Mapped[str] = mapped_column(String, unique=True, nullable=False)
+    expires_at: Mapped[datetime] = mapped_column(DateTime, nullable=False)
+
+    user: Mapped["User"] = relationship(back_populates="refresh_tokens")
