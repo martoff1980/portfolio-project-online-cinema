7744a9cb3e88f33fa6501f277127300b6a1d06d6
martoff <martoff@ukr.net>
2026-07-26 13:44:26 +0300

    apply ConfigDict to all schemas

--- src/schemas/payments.py
+++ src/schemas/payments.py
@@ -1,19 +1,20 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 from datetime import datetime
 from typing import List, Optional
 from src.models.payments import PaymentStatusEnum
 
 
 class PaymentItemResponse(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
     id: int
     order_item_id: int
     price_at_payment: float
 
-    class Config:
-        from_attributes = True
-
 
 class PaymentResponse(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
     id: int
     user_id: int
     order_id: int
@@ -22,6 +23,3 @@ class PaymentResponse(BaseModel):
     amount: float
     external_payment_id: Optional[str]
     items: List[PaymentItemResponse]
-
-    class Config:
-        from_attributes = True
