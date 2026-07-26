bdeddb10f9e9108352eef7e060ed77cf93e203ff
martoff <martoff@ukr.net>
2026-07-26 13:13:43 +0300

    apply ConfigDict to all schemas

--- src/schemas/orders.py
+++ src/schemas/orders.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 from datetime import datetime
 from typing import List
 from src.models.orders import OrderStatusEnum
@@ -6,25 +6,23 @@ from src.schemas.movies import MovieResponse
 
 
 class OrderItemResponse(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
     id: int
     movie_id: int
     price_at_order: float
     movie: MovieResponse
 
-    class Config:
-        from_attributes = True
-
 
 class OrderResponse(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
     id: int
     user_id: int
     created_at: datetime
     status: OrderStatusEnum
     total_amount: float
     items: List[OrderItemResponse]
-
-    class Config:
-        from_attributes = True
 
 
 class CheckoutResponse(BaseModel):
