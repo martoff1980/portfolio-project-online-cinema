e858d97f27e6612225972129aecb72cd499e5ae9
martoff <martoff@ukr.net>
2026-07-26 13:30:41 +0300

    modified routes

--- src/routes/payments.py
+++ src/routes/payments.py
@@ -1,5 +1,6 @@
 import os
 import stripe
+from decimal import Decimal
 from typing import List, Optional
 from fastapi import (
     APIRouter,
@@ -9,7 +10,7 @@ from fastapi import (
     Request,
     status
 )
-from sqlalchemy import select, Numeric
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import selectinload
 
@@ -45,7 +46,7 @@ async def stripe_webhook(
         )
     except ValueError:
         raise HTTPException(status_code=400, detail="Invalid payload")
-    except stripe.error.SignatureVerificationError:
+    except stripe.error.SignatureVerificationError:  # type: ignore[attr-defined]
         raise HTTPException(status_code=400, detail="Invalid signature")
 
     # Stripe sends a webhook event when a checkout session
@@ -57,7 +58,7 @@ async def stripe_webhook(
         external_payment_id = session["id"]
         # Convert cents in dollars
         amount_paid = (
-            Numeric(session["amount_total"]) / 100
+            Decimal(session["amount_total"]) / Decimal("100")
         )
 
         # Search order in the database by ID and load its items
@@ -83,7 +84,6 @@ async def stripe_webhook(
             db.add(payment)
             await db.flush()
 
-            # 4. Создаем детализацию платежа для каждой позиции
             # Create details for each item the order
             for order_item in order.items:
                 payment_item = PaymentItem(
